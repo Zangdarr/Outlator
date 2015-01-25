5c721e9f505f1782a03e0136b7226fdc54b23ed8
Zangdarr <alexandre.verkyndt@gmail.com>
2015-01-25 14:46:29 +0100

    Addition of a function to choose the mode.
    The user can choose edition mode or reading mode. This function is safe.

--- project/Interactions_with_user.py
+++ project/Interactions_with_user.py
@@ -11,3 +11,17 @@ class Interactions_with_user:
         '''
         Empty        
         '''
+
+
+    def choose_your_mode(self):
+        '''
+        Ask the user about the action he want to do : edition or reading.
+        '''
+        print('Choose between edition mode or reading mode (e/r)')
+        while(True):  
+            mode = input('Your choice :')
+            if(mode !='e' and mode !='r' and mode !='edition' and mode !='reading' and mode !='edit' and mode !='read'):
+                print('Error : please choose in e/edit/edition or r/read/reading')
+                continue
+            break
+        return mode[0]
