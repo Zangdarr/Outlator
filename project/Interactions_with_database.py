2575345a7aaea0847b547a6853c0a32ff26d26ec
Zangdarr <alexandre.verkyndt@gmail.com>
2015-01-25 15:01:23 +0100

    Addition of a class which will take care of the interactions with the database.

---
+++ project/Interactions_with_database.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 25 14:58:26 2015
+
+@author: alexandre Verkyndt
+"""
+
+class Interactions_with_database:
+    
+    def __init__(self):
+        '''
+        Empty
+        '''
