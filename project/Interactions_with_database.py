5f490e84a33d347ba940a8d892e9c7cf51dac4d0
Zangdarr <alexandre.verkyndt@gmail.com>
2015-01-25 15:30:50 +0100

    Addition of the class description and the empty constructor.

--- project/Interactions_with_database.py
+++ project/Interactions_with_database.py
@@ -4,3 +4,11 @@ Created on Sun Jan 25 14:58:26 2015
 
 @author: alexandre Verkyndt
 """
+
+
+class Interactions_with_database:
+    
+    def __init__(self):
+        '''
+        Empty
+        '''
