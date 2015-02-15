1091d210b41fd676ff8f2c0f1cb07237b0f30bf4
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-15 17:22:17 +0100

    Mode.py : specification of the behavior of the edition mode.
    After the user choose the edition mode. This one propose him a list of action he can do.

--- project/Mode.py
+++ project/Mode.py
@@ -6,9 +6,18 @@ Created on Sun Feb 15 16:58:55 2015
 """
 
 class Mode_edition:
+    #Attribut
+
+    #when @notfinish will be false the program will leave the edition mode    
+    self.notfinish = True
     
     def edition_mode(self):
         print("You are in the edition mode.\n\n")
+        #until the user want to leave the edition mode
+        while(notfinish):
+            #the user choose the action he want to do
+            action = choose_action(self)
+            #run the action
         
         
 class Mode_reading:
