a8adb00e5a4149de99a6da7b80dfc2da9ba5ced1
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-15 16:38:52 +0100

    Main.py : addition of the edition mode function.
    This function will be call when the user choose the edition mode.
    It will group the facilities to allow the user to edit the database.

--- project/main.py
+++ project/main.py
@@ -5,7 +5,10 @@
 
 import Interactions
 
-
+def edition_mode(self):
+    print("You are in the edition mode.\n\n")
+    
+    
 if __name__ == '__main__':
     
     to_user = Interactions.Interactions_with_user()
