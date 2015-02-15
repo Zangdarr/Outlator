dd7258ad4ae54a45ad01f719b82f184e8013d2a0
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-15 17:13:11 +0100

    main.py : Adaptation of the function mode_launcher() to the new architecture of file.

--- project/main.py
+++ project/main.py
@@ -4,9 +4,22 @@
 """
 
 import Interactions
+import Mode
 
 def mode_launcher(mode):
     print("The mode you choosed will be launch.\n\n")
+
+    if(mode == 'e'):
+        mode_class = Mode.Mode_edition()
+        mode_class.edition_mode()
+    elif(mode == 'r'):
+        mode_class = Mode.Mode_reading()
+        mode_class.reading_mode()
+    else:
+        print("The mode you choose don't existing. Aborting.")
+    
+    print("The program will close.")
+        
 
 
     
